5d6cd3882965c17a560449792026189c5f879fcb
drgoshm <drgosh.m@gmail.com>
2019-04-01 16:38:14 +0300

    Update goodCode.js

--- goodCode.js
+++ goodCode.js
@@ -1,11 +1,11 @@
-/* Math.round */
-
+/* MATH.round */
 var rounded = (0.5 + somenum) << 0;
 // or
 var rounded = (0.5 + somenum) | 0;
 
-/* Math.floor */
 
+
+/* MATH.floor */
 var float = 367.9987;
 
 float | 0 //367
@@ -14,3 +14,11 @@ float >> 0 //367
 float >>> 0 //367
 // nice looks
 ~~float;
+
+
+/* LOOPs */
+//good: length cached 
+for (var i = 0, l = arr.length; i < l; i++) {}
+
+//backwards forloop + caching the length - the BEST
+for (var length = arr.length, i = length - 1; i >= 0; --i) {}
